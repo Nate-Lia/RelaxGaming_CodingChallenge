fdb2bea612e4ea27287f9d65041f2659d47f56dc
Nate-Lia <nthanlia3@gmail.com>
2023-06-23 00:38:14 +0200

    Two Dice Game Finished

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
-import java.util.Random;
+
 public class Main {
     private static int totalOneDieAmount;
+    private static int totalTwoDieAmount;
     private int slotsBet = 10;
 
-    private static Random rand = new Random();
+
     public static void main(String[] args) {
         OneDiceGame();
+        System.out.println("----------------------------------------------");
+        TwoDiceGame();
     }
 
     /**
@@ -22,45 +25,43 @@ public class Main {
                 if(diceNumber == 6 && !sixRolled){
                     sixRolled = true;
                     totalOneDieAmount += 1;
-                    System.out.println("Win");
                 }
 
                 if(rollCounter == 3 && !sixRolled){
                     totalOneDieAmount -= 1;
-                    System.out.println("Lose");
                 }
                 else if(rollCounter == 3 && sixRolled){
                     sixRolled = false;
                 }
             }
         }
-        System.out.println("Total Payout: $" + totalOneDieAmount);
+        System.out.println("One Dice Game Total Payout: $" + totalOneDieAmount);
     }
 
     public static void TwoDiceGame(){
-        /*int dice1Number; // the number the die rolled
-        int dice2Number;
-        boolean sixRolled = false; // checks if a 6 has been rolled
+        int dice1Number; // the number of dice 1
+        int dice2Number; // the number of dice 2
+        boolean doubleSixRolled = false; // checks if a 6 has been rolled by both dice
         for(int i = 0; i < 1000000; i++){
-            for(int rollCounter = 0; rollCounter < 4; rollCounter++){
-                diceNumber = (int) (Math.random() * 6 + 1);
-                diceNumber += 1; // randomize between 1-6
+            for(int rollCounter = 0; rollCounter < 24; rollCounter++){
+                dice1Number = (int) (Math.random() * 6 + 1);
+                dice1Number += 1; // randomize between 1-6
+                dice2Number = (int) (Math.random() * 6 + 1);
+                dice2Number += 1; // randomize between 1-6
 
-                if(diceNumber == 6 && !sixRolled){
-                    sixRolled = true;
-                    totalOneDieAmount += 1;
-                    System.out.println("Win");
+                if(dice1Number == 6 && dice2Number==6  && !doubleSixRolled){
+                    doubleSixRolled = true;
+                    totalTwoDieAmount += 1;
                 }
 
-                if(rollCounter == 3 && !sixRolled){
-                    totalOneDieAmount -= 1;
-                    System.out.println("Lose");
+                if(rollCounter == 3 && !doubleSixRolled){
+                    totalTwoDieAmount -= 1;
                 }
-                else if(rollCounter == 3 && sixRolled){
-                    sixRolled = false;
+                else if(rollCounter == 3 && doubleSixRolled){
+                    doubleSixRolled = false;
                 }
             }
         }
-        System.out.println("Total Payout: $" + totalOneDieAmount);*/
+        System.out.println("Two Dice Game Total Payout: $" + totalTwoDieAmount);
     }
 }
