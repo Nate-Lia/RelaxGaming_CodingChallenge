5dc251acbe2d3401cbd072ff4347fc249abc76aa
Nate-Lia <nthanlia3@gmail.com>
2023-06-28 11:05:25 +0200

    bonus game functionality completed

--- src/SlotsGameSimulator.java
+++ src/SlotsGameSimulator.java
@@ -1,17 +1,6 @@
 import java.util.Map;
 
 public class SlotsGameSimulator {
-    static class Coin{
-        int value;
-        int weight;
-
-        public Coin(int value, int weight){
-            this.value = value;
-            this.weight = weight;
-
-        }
-    }
-
     static String[][] slotMatrix = new String[3][3];
     static String[] slotColumn = {"W1","H1","H2","H3","L1","L2","L3","L4", "B1"};
     static Map<String, Integer> slotPayouts = Map.of(
@@ -27,8 +16,9 @@ public class SlotsGameSimulator {
     );
 
     static boolean isWinner = false;
-
     static int bonusCounter = 0;
+    static int bet = 10;
+    static int totalPayout = 0;
 
     public static void main(String[] args){
         SpinSlots();
@@ -51,46 +41,55 @@ public class SlotsGameSimulator {
                 System.out.print(slotMatrix[i][j] + " ");
                 //check if the slot is a winner if the row has 3 of the same symbol
                 if(j == 2){
-                    if(isWinningCombination(slotMatrix[i][0],  slotMatrix[i][1], slotMatrix[i][2]) && !isWinner){
+                    if(IsWinningCombination(slotMatrix[i][0],  slotMatrix[i][1], slotMatrix[i][2]) && !isWinner){
                         if(slotMatrix[i][0].equals("W1") && !slotMatrix[i][1].equals("W1")){
                             //print payout of second symbol
-                            System.out.print("Winner! Payout: $" + getPayout(slotMatrix[i][1]));
+                            System.out.print("Winner! Payout: $" + GetPayout(slotMatrix[i][1]));
+                            totalPayout += GetPayout(slotMatrix[i][1]);
                         }
                         else if(slotMatrix[i][1].equals("W1") && !slotMatrix[i][2].equals("W1")){
                             //print payout of third symbol
-                            System.out.print("Winner! Payout: $" + getPayout(slotMatrix[i][2]));
+                            System.out.print("Winner! Payout: $" + GetPayout(slotMatrix[i][2]));
+                            totalPayout += GetPayout(slotMatrix[i][2]);
                         }
                         else{
-                            System.out.print("Winner! Payout: $" + getPayout(slotMatrix[i][0]));
+                            System.out.print("Winner! Payout: $" + GetPayout(slotMatrix[i][0]));
+                            totalPayout += GetPayout(slotMatrix[i][0]);
                         }
                     }
                     //if the row is not a winner, check if the diagonals are winners
                     else if(i == 1){
-                        if(isWinningCombination(slotMatrix[0][0], slotMatrix[1][1], slotMatrix[2][2])){
+                        if(IsWinningCombination(slotMatrix[0][0], slotMatrix[1][1], slotMatrix[2][2])){
                             if(slotMatrix[0][0].equals("W1") && !slotMatrix[1][1].equals("W1")){
                                 //print payout of second symbol
-                                System.out.print("Winner! Payout: $" + getPayout(slotMatrix[1][1]));
+                                System.out.print("Winner! Payout: $" + GetPayout(slotMatrix[1][1]));
+                                totalPayout += GetPayout(slotMatrix[1][1]);
                             }
                             else if(slotMatrix[1][1].equals("W1") && !slotMatrix[2][2].equals("W1")){
                                 //print payout of third symbol
-                                System.out.print("Winner! Payout: $" + getPayout(slotMatrix[2][2]));
+                                System.out.print("Winner! Payout: $" + GetPayout(slotMatrix[2][2]));
+                                totalPayout += GetPayout(slotMatrix[2][2]);
                             }
                             else{
-                                System.out.print("Winner! Payout: $" + getPayout(slotMatrix[0][0]));
+                                System.out.print("Winner! Payout: $" + GetPayout(slotMatrix[0][0]));
+                                totalPayout += GetPayout(slotMatrix[0][0]);
                             }
 
                         }
-                        else if(isWinningCombination(slotMatrix[0][2], slotMatrix[1][1], slotMatrix[2][0])){
+                        else if(IsWinningCombination(slotMatrix[0][2], slotMatrix[1][1], slotMatrix[2][0])){
                             if(slotMatrix[0][2].equals("W1") && !slotMatrix[1][1].equals("W1")){
                                 //print payout of second symbol
-                                System.out.print("Winner! Payout: $" + getPayout(slotMatrix[1][1]));
+                                System.out.print("Winner! Payout: $" + GetPayout(slotMatrix[1][1]));
+                                totalPayout += GetPayout(slotMatrix[1][1]);
                             }
                             else if (slotMatrix[1][1].equals("W1") && !slotMatrix[2][0].equals("W1")){
                                 //print payout of third symbol
-                                System.out.print("Winner! Payout: $" + getPayout(slotMatrix[2][0]));
+                                System.out.print("Winner! Payout: $" + GetPayout(slotMatrix[2][0]));
+                                totalPayout += GetPayout(slotMatrix[2][0]);
                             }
                             else{
-                                System.out.print("Winner! Payout: $" + getPayout(slotMatrix[0][2]));
+                                System.out.print("Winner! Payout: $" + GetPayout(slotMatrix[0][2]));
+                                totalPayout += GetPayout(slotMatrix[0][2]);
                             }
                         }
                     }
@@ -101,8 +100,10 @@ public class SlotsGameSimulator {
                 }
             }
         }
+        //if bonusCounter >= 3 then bonus game is triggered
         if(bonusCounter >= 3){
             System.out.println("\n"+"--BONUS GAME TRIGGERED!--");
+            System.out.println("Payout: $" + (RandomCoin() + totalPayout));
         }
     }
 
@@ -113,7 +114,7 @@ public class SlotsGameSimulator {
      * @param symbol3
      * @return
      */
-    private static boolean isWinningCombination(String symbol1, String symbol2, String symbol3) {
+    private static boolean IsWinningCombination(String symbol1, String symbol2, String symbol3) {
         // Check if the symbols form a winning combination
         return symbol1.equals(symbol2) && (symbol2.equals(symbol3) || symbol3.equals("W1"))
                 || symbol1.equals("W1") && symbol2.equals(symbol3) || symbol1.equals(symbol3) && symbol2.equals("W1")
@@ -126,8 +127,44 @@ public class SlotsGameSimulator {
      * @param symbol
      * @return payout
      */
-    private static int getPayout(String symbol) {
+    private static int GetPayout(String symbol) {
         // Retrieve the payout for the symbol from the payout table
         return slotPayouts.get(symbol);
+    }
+
+    /**
+     * When bonus game is triggered by getting 3 "B1"s, generate a random amount of coins
+     *  A die is rolled as a multiplier
+     * @return random amount of coins
+     */
+    private static int RandomCoin() {
+        int totalWeights = 0;
+        int dieRoll = (int) (Math.random() * 6) + 1;
+        // generate a set random amount of coins using a predetermined weight
+       Map<Integer, Integer> coinMap = Map.of(
+               10, 100,
+                20, 50,
+               40, 30,
+               50, 20,
+               151, 10,
+               220, 5,
+               260, 4,
+               300, 3,
+               350, 2
+       );
+         for(int weight : coinMap.keySet()){
+             totalWeights += weight;
+         }
+        int randomWeight = (int) (Math.random() * totalWeights);
+         int cumulativeWeight = 0;
+        //check randomWeight if it is less than the weight of the coinMap key, then return the value
+        for(int weight : coinMap.keySet()){
+            cumulativeWeight += weight;
+            if(randomWeight < cumulativeWeight){
+                return coinMap.get(weight) * dieRoll * bet;
+            }
+        }
+        //if the randomWeight is greater than the cumulativeWeight, return the last value
+        return coinMap.get(totalWeights) * dieRoll *  bet;
     }
 }
